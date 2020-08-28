5d2026583b1ab03bdf0e0e6b232f25c5581d14fd
Johnson Wang <freelance@rfphoenix.com>
2020-08-27 22:17:38 -0700

    Initial Commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(),
+    );
+  }
+}
